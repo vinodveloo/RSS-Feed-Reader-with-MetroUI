c835ae594a8e4d73aae3b3a6ec2ff800e884c5c8
vinodveloo <31234177+vinodveloo@users.noreply.github.com>
2017-12-03 14:32:51 +0800

    Add files via upload

---
+++ RSS Feed Reader/RSS Feed Reader/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace RSS_Feed_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rSSFeedDataSet = new RSS_Feed_Reader.RSSFeedDataSet();
+            this.rSSLinkBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.rSS_LinkTableAdapter = new RSS_Feed_Reader.RSSFeedDataSetTableAdapters.RSS_LinkTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rSSLinkDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rSSFeedDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rSSLinkBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add RSS Feed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(20, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 423);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Feed Control";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(235, 60);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(480, 423);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Beige;
+            this.label1.Location = new System.Drawing.Point(680, 486);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "V 1.0";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.rSSLinkDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.rSSLinkBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(209, 276);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // rSSFeedDataSet
+            // 
+            this.rSSFeedDataSet.DataSetName = "RSSFeedDataSet";
+            this.rSSFeedDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // rSSLinkBindingSource
+            // 
+            this.rSSLinkBindingSource.DataMember = "RSS_Link";
+            this.rSSLinkBindingSource.DataSource = this.rSSFeedDataSet;
+            // 
+            // rSS_LinkTableAdapter
+            // 
+            this.rSS_LinkTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 30;
+            // 
+            // rSSLinkDataGridViewTextBoxColumn
+            // 
+            this.rSSLinkDataGridViewTextBoxColumn.DataPropertyName = "RSS_Link";
+            this.rSSLinkDataGridViewTextBoxColumn.HeaderText = "RSS_Link";
+            this.rSSLinkDataGridViewTextBoxColumn.Name = "rSSLinkDataGridViewTextBoxColumn";
+            this.rSSLinkDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(735, 503);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "RSS Feed Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rSSFeedDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rSSLinkBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private RSSFeedDataSet rSSFeedDataSet;
+        private System.Windows.Forms.BindingSource rSSLinkBindingSource;
+        private RSSFeedDataSetTableAdapters.RSS_LinkTableAdapter rSS_LinkTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rSSLinkDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ RSS Feed Reader/RSS Feed Reader/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+//Library for reading the XML input
+using System.Xml;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.ServiceModel.Syndication;
+using System.Data.SqlClient;
+
+
+
+namespace RSS_Feed_Reader
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        //Connection String to DataSource
+
+        SqlConnection con = new SqlConnection(@"Data Source=W10C42S6H2\SQLEXPRESS;Initial Catalog=RSSFeed;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'rSSFeedDataSet.RSS_Link' table. You can move, or remove it, as needed.
+            this.rSS_LinkTableAdapter.Fill(this.rSSFeedDataSet.RSS_Link);
+
+
+            update_datagrid();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Initialization of XML reader
+
+                XmlReader RSS_readerxml = XmlReader.Create(textBox1.Text);
+                SyndicationFeed RSS_feed = SyndicationFeed.Load(RSS_readerxml);
+
+                TabPage RSS_Tab = new TabPage(RSS_feed.Title.Text);
+             
+
+                //Adding new tab for each feed
+                tabControl1.TabPages.Add(RSS_Tab);
+
+
+                ListBox RSS_List = new ListBox();
+
+                RSS_Tab.Controls.Add(RSS_List);
+
+                RSS_List.Dock = DockStyle.Fill;
+
+                RSS_List.HorizontalScrollbar = true;
+
+                foreach(SyndicationItem RSS_Items in RSS_feed.Items)
+                {
+                    //Title
+                    RSS_List.Items.Add(RSS_Items.Title.Text);
+                     RSS_List.Items.Add("______________________");
+
+                    //Summary 
+                    RSS_List.Items.Add(RSS_Items.Summary.Text);
+                    RSS_List.Items.Add("======================");
+
+
+
+
+                }
+
+
+
+
+
+
+
+            }
+            
+
+            catch(Exception ex)
+            {
+                MessageBox.Show( ex.Message);
+            }
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+
+            //Save command
+            cmd.CommandText = "insert into RSS_Link values ('" +textBox1.Text+ "')";
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            update_datagrid();
+
+            //User Alert
+            MessageBox.Show("Updated Succesfully");
+        }
+
+        public void update_datagrid()
+        {
+            //To display the data on load or button click 
+
+            con.Open();
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from RSS_Link";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
